beafee0c5963efd442e997c2f14a8b94c6cb53fb
Avinash Goen <agoen97@gmail.com>
2023-05-29 18:46:35 +0530

    CSV Writer Update

--- utils/store.py
+++ utils/store.py
@@ -1,14 +1,21 @@
 import csv
 from datetime import datetime
 
+
 def record_attendace(output):
     now = datetime.now()
     current_date = now.strftime("%Y-%m-%d")
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 
-    f = open(f"static/attendance/{current_date}.csv", "w+", newline="")
+    file_path = f"static/attendance/{current_date}.csv"
+    f = open(file_path, "w+", newline="")
+    count = sum(1 for _ in f)
+
     lnwriter = csv.writer(f)
+    if count < 1:
+        lnwriter.writerow(["DateTime", "UniqueID"])
+
     for entry in output:
-        lnwriter.writerow([date_time, entry['label']])
+        lnwriter.writerow([date_time, entry["label"]])
 
     f.close()
